01737af1fcb66ffa19e4f697b770983dbcd81ae9
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-11-10 15:25:30 -0500

    Initial commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+console.log("test"); //dfd
+
+const prompt = require('prompt-sync')();
+
+//var fname = prompt("Enter your first name: ");
+//var lname = prompt("Enter your last name: ");
+
+var myName = "nate";
+let myLastName = "agcaoili";
+const pi = 3.14;
+var testNum = 2;
+testNum++;
+var string = "\nhi\n\'\"\\\thi\vhi"
+console.log("'single' quote");
+console.log('"double" quote');
+console.log(`'single' and "double" quote`);
+//console.log("Your full name is",fname,lname);
+console.log(string);
---
+++ testing.js
@@ -0,0 +1,6 @@
+var fName = "John";
+var lName = "Doe";
+var fullName = fName + " " + lName;
+var fullNameLength = fullName.length;
+console.log(fullName[0]);
+console.log(fullName);
